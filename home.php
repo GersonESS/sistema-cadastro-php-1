7b5c2807048ee36bfc3d2f2b71a83a5eda38ecc8
Gerson Santos <gersones@outlook.com>
2023-10-20 00:18:23 -0300

    a

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SITE GES</title>
+    <style>
+        body{
+            font-family: Arial, Hev
+
+             parei 4:44
+        }
+
+
+    </style>
+</head>
+<body>
+    <h1>Se invreva No Canal</h1>
+    <div class="box">
+        <a href="">Login</a>
+        <a href="">Cadastre-se</a>
+</body>
+</html>
